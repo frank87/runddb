05c0d4315f7dbbd26bec39394c32227d13b33367
Frank Wijnans <frank.wijnans@piratenpartij.nl>
2022-01-20 17:07:07 +0100

    added overlay

--- flake.nix
+++ flake.nix
@@ -1,41 +1,60 @@
 {
-  description = "A Rust web server including a NixOS module";
+	description = "A Rust web server including a NixOS module";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+	# Nixpkgs / NixOS version to use.
+	inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
 
-  outputs = { self, nixpkgs }:
-    {
-      # The default package for 'nix build'. This makes sense if the
-      defaultPackage."x86_64-linux" = 
-	with import nixpkgs{ system = "x86_64-linux"; };
-        let
-		 deps = import ./rebar-deps.nix { inherit (pkgs) fetchHex fetchFromGitHub; };
-	in
-	pkgs.stdenv.mkDerivation{
-		name = "runddb";
-		src = self;
+	outputs = { self, nixpkgs }:
+	{
+		# The default package for 'nix build'. This makes sense if the
+		defaultPackage."x86_64-linux" = 
+			with import nixpkgs{ system = "x86_64-linux"; };
+			let
+				deps = import ./rebar-deps.nix { inherit (pkgs) fetchHex fetchFromGitHub; };
+			in
+			pkgs.stdenv.mkDerivation{
+				name = "runddb";
+				src = self;
 
-		buildPhase = ''
-			mkdir -p _checkouts
-			${toString (pkgs.lib.mapAttrsToList (k: v: ''
-			cp -R --no-preserve=mode ${v} _checkouts/${k}
-			'') deps)}
-			HOME=. rebar3 tar
-		'';
+				buildPhase = ''
+				mkdir -p _checkouts
+				${toString (pkgs.lib.mapAttrsToList (k: v: ''
+				cp -R --no-preserve=mode ${v} _checkouts/${k}
+				'') deps)}
+				HOME=. rebar3 tar
+				'';
 
-		installPhase = ''
-			mkdir -p $out
-			tar -xzvf _build/default/rel/*/*.tar.gz  -C $out/
-		'';
+				installPhase = ''
+				mkdir -p $out
+				tar -xzvf _build/default/rel/*/*.tar.gz  -C $out/
+				'';
 
 
-		buildInputs = [ openssl rebar3 gnutar ];
+				buildInputs = [ openssl rebar3 gnutar ];
+			};
+
+		# A NixOS overlay.
+		overlay = final: prev: {
+			runddb = self.defaultPackage."x86_64-linux";
+
+			config = {
+				systemd.services.runddb = {
+					description = "Online REST database for cattle";
+
+					wantedBy="multi-user.target";
+
+					serviceConfig = {
+						Type="notify";
+						User="runddb";
+
+						ExecStart="${self.runddb}/bin/runddb daemon";
+						WatchdogSec="10s";
+						Restart="on-failure";
+					};
+
+				};
+
+			};
+		};
 	};
-
-      # A NixOS overlay.
-      overlay = final: prev: {
-	runddb = self.defaultPackage."x86_64-linux";
-      };
-    };
 }
